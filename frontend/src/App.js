e5e626dfe4eabb837863f77f78c2c4f422038619
Sehansi <117649168+Sehansi@users.noreply.github.com>
2026-02-23 13:18:32 +0530

    change the file name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Register from "./pages/Register";
@@ -15,23 +16,25 @@ import FurnitureCustomizer from "./pages/FurnitureCustomizer";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/viewer" element={<Viewer3D />} />
-        <Route path="/designer" element={<Designer />} />
-        <Route path="/ai-designer" element={<AIDesigner />} />
-        <Route path="/furniture-customizer" element={<FurnitureCustomizer />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/viewer" element={<Viewer3D />} />
+          <Route path="/designer" element={<Designer />} />
+          <Route path="/ai-designer" element={<AIDesigner />} />
+          <Route path="/furniture-customizer" element={<FurnitureCustomizer />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
